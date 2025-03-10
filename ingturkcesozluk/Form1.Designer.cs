4bd53250e77a074d6f5df754cf729ea40495b6f0
zeyne <zeyne@ZETUS>
2025-03-10 17:26:34 +0300

    Proje dosyası ekle.

---
+++ ingturkcesozluk/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ingturkcesozluk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İngilizce :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Türkçe:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 56);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(234, 132);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 29);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.Location = new System.Drawing.Point(57, 210);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(211, 210);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "GÜNCELLE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(361, 210);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "SİL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(78, 301);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(571, 180);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PaleGreen;
+            this.button4.Location = new System.Drawing.Point(508, 210);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 50);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Rastgele Kelime Getir";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.PaleGreen;
+            this.button5.Location = new System.Drawing.Point(508, 132);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 50);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Cümle Ekle";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(752, 518);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ingturkcesozluk/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Drawing.Text;
+
+
+namespace ingturkcesozluk
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=ZETUS\\SQLZETUS;Initial Catalog=ingturkce;Integrated Security=True;");
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand silkomutu = new SqlCommand("delete from sozluk where ing ='" + textBox1.Text + "'", baglanti);
+                silkomutu.ExecuteNonQuery();
+                baglanti.Close();
+
+                MessageBox.Show("Sözcük veritabanından silindi", "Veri Tabanı İşlemleri");
+                textBox1.Clear();
+                textBox2.Clear();
+                listBox1.Items.Clear();
+            }
+            catch (Exception aciklama)
+            {
+
+                MessageBox.Show(aciklama.Message, "Veritabanı İşlemleri");
+                baglanti.Close();
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand guncellesorgu = new SqlCommand("insert  into sozluk (ing,türkce) values ('" + textBox1.Text + "','" + textBox2.Text + "')", baglanti);
+                guncellesorgu.ExecuteNonQuery();
+                baglanti.Close();
+
+                MessageBox.Show("Sözcük  Veri Tabanına Eklendi ...", "Veri Tabanı İşlemleri");
+                textBox1.Clear();
+                textBox2.Clear();
+
+
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message, "Veri Tabanı İşlemleri");
+                baglanti.Close();
+            }
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                baglanti.Open();
+                SqlCommand guncellesorgu = new SqlCommand("update sozluk set türkce ='" + textBox2.Text + "' where ing ='" + textBox1.Text + "'", baglanti);
+                guncellesorgu.ExecuteNonQuery();
+
+
+                MessageBox.Show("Sözcük  Veri Tabanında Güncellendi...", "Veri Tabanı İşlemleri");
+                textBox1.Clear();
+                textBox2.Clear();
+
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message, "Veri Tabanı İşlemleri");
+                baglanti.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                baglanti.Open();
+                SqlCommand aramasorgu = new SqlCommand("select ing,türkce from sozluk where ing like'" + textBox1.Text + "%'", baglanti);
+
+                SqlDataReader read = aramasorgu.ExecuteReader();
+
+                while (read.Read())
+                {
+                    listBox1.Items.Add(read["ing"].ToString() + "=" + read["türkce"].ToString());
+                }
+                baglanti.Close();
+            }
+            catch (Exception aciklama)
+            {
+                MessageBox.Show(aciklama.Message, "veri tabanı işlemleri");
+                baglanti.Close();
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand vericekme = new SqlCommand("SELECT TOP 1 * FROM sozluk ORDER BY NEWID();", baglanti);
+            SqlDataReader reader = vericekme.ExecuteReader();
+
+            try
+            {
+                if (reader.Read())
+                {
+                    listBox1.Items.Add(reader["ing"].ToString() + " = " + reader["türkce"].ToString());
+
+                }
+
+
+                baglanti.Close();
+                reader.Close();
+            }
+            catch (Exception aciklama)
+            {
+
+                MessageBox.Show(aciklama.Message, "Veri Tabanı İşlemleri");
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+
+            string ingilizceKelime = textBox1.Text.Trim();  // İlk TextBox'tan kelimeyi al
+            string cumle = textBox2.Text.Trim();             // İkinci TextBox'tan cümleyi al
+
+            // Eğer kelime ve cümle boş değilse işlemi yap
+            if (!string.IsNullOrEmpty(ingilizceKelime) && !string.IsNullOrEmpty(cumle))
+            {
+
+                baglanti.Open();
+
+                // 1. Adım: Veritabanındaki kelimeyi bul ve cümleyi güncelle
+                string updateQuery = "UPDATE sozluk SET cumle = @cumle WHERE ing = @ingilizceKelime";
+                SqlCommand updateCmd = new SqlCommand(updateQuery, baglanti);
+                updateCmd.Parameters.AddWithValue("@cumle", cumle);
+                updateCmd.Parameters.AddWithValue("@ingilizceKelime", ingilizceKelime);
+
+                int rowsAffected = updateCmd.ExecuteNonQuery();  // Güncelleme işlemi yap ve kaç satır etkilendiğini kontrol et
+                baglanti.Close();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Cümle başarıyla güncellendi!");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("İlgili kelime bulunamadı.");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen hem kelimeyi hem de cümleyi girin.");
+            }
+
+        }
+    }
+}
+// there are more columns in the ınsert statement than values specified in the values clause. the number of values in the values clause must match the number of columns specified in the ınsert statement.
